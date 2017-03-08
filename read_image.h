0adeb8815e836d57f30e2a9d72109f52645bd274
kun150kun <kun150kun111@gmail.com>
2017-03-09 00:34:19 +0800

    Delete read_image.h

--- read_image.h
+++
@@ -1,9 +0,0 @@
-#ifndef READ_IMAGE_H
-#define READ_IMAGE_H
-
-#include "image.h"
-
-int read_image(char * filename,Image **Im);
-void buffer_to_Im(Image* Im, unsigned char** buffer, int line);
-
-#endif
